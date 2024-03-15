3d497768393755083d472132f8fe364150e8ad61
John Fletcher <john@bunbury28.plus.com>
2024-03-15 20:26:07 +0000

    Update MCUFRIEND_clock.ino
    Tidy up the code

--- MCUFRIEND_clock/MCUFRIEND_clock.ino
+++ MCUFRIEND_clock/MCUFRIEND_clock.ino
@@ -1,20 +1,10 @@
 // MCUFRIEND_clock.ino starting my own example
 // This is now a copy of the TFT_Clock example from the TFT_ILI9341 library
 // Using MCUDRIEND_kbv instead of the ELEGOO version
-// I am now making an Arduino DUE version to upload the TFT display to the Process application
+// This code will compile on an Arduino UNO.
 /////////////////////////////////////////////////////////////
-// IMPORTANT: ELEGOO_TFTLCD LIBRARY MUST BE SPECIFICALLY
-// CONFIGURED FOR EITHER THE TFT SHIELD OR THE BREAKOUT BOARD.
-// SEE RELEVANT COMMENTS IN Elegoo_TFTLCD.h FOR SETUP.
-//Technical support:goodtft@163.com
-
-#ifdef ARDUINO_SAM_DUE
-#define ARDUINO_DUE
-#endif
 
 #define USE_Elegoo_SHIELD_PINOUT
-//#include <Elegoo_GFX.h>    // Core graphics library
-//#include <Elegoo_TFTLCD.h> // Hardware-specific library
 #include <MCUFRIEND_kbv.h>
 
 // The control pins for the LCD can be assigned to any digital or
@@ -26,19 +16,6 @@
 #define LCD_RD A0 // LCD Read goes to Analog 0
 
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
-
-// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
-// For the Arduino Uno, Duemilanove, Diecimila, etc.:
-//   D0 connects to digital pin 8  (Notice these are
-//   D1 connects to digital pin 9   NOT in order!)
-//   D2 connects to digital pin 2
-//   D3 connects to digital pin 3
-//   D4 connects to digital pin 4
-//   D5 connects to digital pin 5
-//   D6 connects to digital pin 6
-//   D7 connects to digital pin 7
-// For the Arduino Mega, use digital pins 22 through 29
-// (on the 2-row header at the end of the board).
 
 // Assign human-readable names to some common 16-bit color values:
 #define	BLACK   0x0000
@@ -52,17 +29,7 @@
 
 #define GREY    0x5AEB
 
-
-//Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
-// If using the shield, all control and data lines are fixed, and
-// a simpler declaration can optionally be used:
-//Elegoo_TFTLCD tft;
 MCUFRIEND_kbv tft;
-
-
-#ifdef ARDUINO_SAM_DUE
-//#undef y0
-#endif
 
 int count = 0;
 
@@ -165,12 +132,6 @@ void setup(void) {
   }
 
   tft.fillCircle(120, 121, 3, TFT_WHITE);
-  // Draw text at position 120,260 using fonts 4
-  // Only font numbers 2,4,6,7 are valid. Font 6 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : . - a p m
-  // Font 7 is a 7 segment font and only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : .
-  //tft.drawCentreString("Time flies",120,260,4); // not available.
-
-  //delay(5000);
  
 }
 
@@ -223,55 +184,6 @@ void loop(void) {
     tft.fillCircle(120, 121, 3, TFT_RED);
   }
 
-  /*
-  tft.fillScreen(BLACK);
-  unsigned long start = micros();
-  tft.setCursor(0, 0);
-  
-  tft.setTextColor(RED);  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.println(01234.56789);
-  tft.println(0xDEADBEEF, HEX);
-  tft.println();
-  tft.println();
-  tft.setTextColor(GREEN); tft.setTextSize(2);
-   tft.println("Hello World!");
-  tft.println(01234.56789);
-  tft.println(0xDEADBEEF, HEX);
-  tft.println();
-  tft.println();
-  
-  tft.setTextColor(BLUE);    tft.setTextSize(3);
-   tft.println("Hello World!");
-  tft.println(01234.56789);
-  tft.println(0xDEADBEEF, HEX);
-  
-  tft.setTextColor(WHITE);    tft.setTextSize(4);
-   tft.println("Hello!");
-   tft.setTextColor(YELLOW);    tft.setTextSize(5);
-   tft.println("Hello!");
-   tft.setTextColor(RED);    tft.setTextSize(6);
-   tft.println("Hello!");
-  tft.println();
-  tft.println();
-  */
-  /*
-  tft.println();
-  tft.setTextColor(GREEN);
-  tft.setTextSize(5);
-  tft.println("Groop");
-  tft.setTextSize(2);
-  tft.println("I implore thee,");
-  tft.setTextSize(1);
-  tft.println("my foonting turlingdromes.");
-  tft.println("And hooptiously drangle me");
-  tft.println("with crinkly bindlewurdles,");
-  tft.println("Or I will rend thee");
-  tft.println("in the gobberwarts");
-  tft.println("with my blurglecruncheon,");
-  tft.println("see if I don't!");*/
-  //delay(1000);delay(1000);delay(1000);delay(1000);delay(1000);
-  //Serial.println(count++);
 }
 
 static uint8_t conv2d(const char* p) {
